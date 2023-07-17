d87a9917b5259827c2d05d226dd3fd5f4f23c8dd
YAMANOE <139040160+YAMANOE@users.noreply.github.com>
2023-07-18 01:27:46 +0300

    frist commint

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Homework2
+### creat a dictionary that contains your name ,age ,address,phone number and email for 5 person .
+
+also print name and email then change your age
+and print it again 
  
---
+++ app.py
@@ -0,0 +1,24 @@
+person1={"name":"yaman","age":20,"adress":"irbid","phone number":779701230,"email":"yamanobiedat108@gmail.com"}
+person2={"name":"ahmad","age":22,"adress":"amman","phone number":1111111111,"email":"ahmad@gmail.com"}
+person3={"name":"mohamad","age":30,"adress":"Aqaba","phone number":22222222,"email":"mohamad@gmail.com"}
+person4={"name":"kaled","age":40,"adress":"as-Salt","phone number":33333333,"email":"kaled@gmail.com"}
+person5={"name":"waleed","age":12,"adress":"Ma'an","phone number":444444444,"email":"waleed@gmail.com"}
+#----------------------------------------------
+print(person1["name"],person1["email"])
+print(person2["name"],person2["email"])
+print(person3["name"],person3["email"])
+print(person4["name"],person4["email"])
+print(person5["name"],person5["email"])
+print(50*'-')
+#-----------------------------------------------
+person1["age"]=24
+person2["age"]=33
+person3["age"]=23
+person4["age"]=26
+person5["age"]=38
+#-----------------------------------------------
+print(person1["age"])
+print(person2["age"])
+print(person3["age"])
+print(person4["age"])
+print(person5["age"])
